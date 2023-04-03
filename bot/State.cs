ffe015c1ab87b00db6994a581ce67dd9789714ec
Соколов Алексей <av.sok@skbkontur.ru>
2023-04-03 23:46:45 +0500

    feat: state and state reading

--- bot/State.cs
+++ bot/State.cs
@@ -1,10 +1,92 @@
-﻿namespace bot
+﻿namespace bot;
+
+public class State
 {
-    public class StateInit
+    public readonly Player Me;
+    public readonly Player Enemy;
+    public readonly GameAction[] Actions;
+
+    public State(Player me, Player enemy, GameAction[] actions)
     {
+        Me = me;
+        Enemy = enemy;
+        Actions = actions;
+    }
+}
+
+public class Player
+{
+    public readonly IngredientsVolume Inventory; // ingredient counts
+    public readonly int Score;
+
+    public Player(IngredientsVolume inventory, int score)
+    {
+        Inventory = inventory;
+        Score = score;
+    }
+}
+
+public class GameAction
+{
+    public readonly int Id; // the unique ID of this spell or recipe
+    public readonly GameActionType Type; // in the first league: BREW; later: CAST, OPPONENT_CAST, LEARN, BREW
+    public readonly IngredientsVolume Delta; // ingredients changes
+    public readonly int Price; // the price in rupees if this is a potion
+    public readonly int TomeIndex; // in the first two leagues: always 0; later: the index in the tome if this is a tome spell, equal to the read-ahead tax; For brews, this is the value of the current urgency bonus
+    public readonly int TaxCount; // in the first two leagues: always 0; later: the amount of taxed tier-0 ingredients you gain from learning this spell; For brews, this is how many times you can still gain an urgency bonus
+    public readonly bool Castable; // in the first league: always 0; later: 1 if this is a castable player spell
+    public readonly bool Repeatable; // for the first two leagues: always 0; later: 1 if this is a repeatable player spell
+
+    public GameAction(
+        int id, 
+        GameActionType type, 
+        IngredientsVolume delta, 
+        int price, 
+        int tomeIndex = 0, 
+        int taxCount = 0, 
+        bool castable = false, 
+        bool repeatable = false)
+    {
+        Id = id;
+        Type = type;
+        Delta = delta;
+        Price = price;
+        TomeIndex = tomeIndex;
+        TaxCount = taxCount;
+        Castable = castable;
+        Repeatable = repeatable;
+    }
+}
+
+public enum GameActionType
+{
+    Brew
+}
+
+public class IngredientsVolume
+{
+    public int ZeroIngredient;
+    public int FirstIngredient;
+    public int SecondIngredient;
+    public int ThirdIngredient;
+
+    public IngredientsVolume(
+        int zeroIngredient, 
+        int firstIngredient, 
+        int secondIngredient, 
+        int thirdIngredient)
+    {
+        ZeroIngredient = zeroIngredient;
+        FirstIngredient = firstIngredient;
+        SecondIngredient = secondIngredient;
+        ThirdIngredient = thirdIngredient;
     }
 
-    public class State
+    public void Apply(IngredientsVolume delta)
     {
+        ZeroIngredient += delta.ZeroIngredient;
+        FirstIngredient += delta.FirstIngredient;
+        SecondIngredient += delta.SecondIngredient;
+        ThirdIngredient += delta.ThirdIngredient;
     }
 }
--- bot/StateReader.cs
+++ bot/StateReader.cs
@@ -1,25 +1,67 @@
-﻿namespace bot
+﻿using System;
+
+namespace bot;
+
+public static class StateReader
 {
-    public static class StateReader
+    public static State ReadState(this ConsoleReader console)
     {
-        public static State ReadState(this ConsoleReader reader)
+        var actionCount = int.Parse(console.ReadLine()); // the number of spells and recipes in play
+        var actions = new GameAction[actionCount];
+        
+        for (int i = 0; i < actionCount; i++)
         {
-            var init = reader.ReadInit();
-            return reader.ReadState(init);
+            var inputs = console.ReadLine().Split(' ');
+            actions[i] = ReadGameAction(inputs);
         }
         
-        // ReSharper disable once InconsistentNaming
-        public static State ReadState(this ConsoleReader Console, StateInit init)
-        {
-            // Copy paste here the code for input turn data
-            return new State();
-        }
+        var myInputs = console.ReadLine().Split(' ');
+        var me = ReadPlayer(myInputs);
+        
+        var enemyInputs = console.ReadLine().Split(' ');
+        var enemy = ReadPlayer(enemyInputs);
+        
+        return new State(me, enemy, actions);
+    }
 
-        // ReSharper disable once InconsistentNaming
-        public static StateInit ReadInit(this ConsoleReader Console)
-        {
-            // Copy paste here the code for initialization input data (or delete if no initialization data in this game)
-            return new StateInit();
-        }
+    private static Player ReadPlayer(string[] inputs)
+    {
+        var inv0 = int.Parse(inputs[0]);
+        var inv1 = int.Parse(inputs[1]);
+        var inv2 = int.Parse(inputs[2]);
+        var inv3 = int.Parse(inputs[3]);
+        var inventory = new IngredientsVolume(inv0, inv1, inv2, inv3);
+
+        var score = int.Parse(inputs[4]); // amount of rupees
+
+        return new Player(inventory, score);
+    }
+
+    private static GameAction ReadGameAction(string[] inputs)
+    {
+        var actionId = int.Parse(inputs[0]); // the unique ID of this spell or recipe
+
+        var actionType =
+            Enum.Parse<GameActionType>(inputs[1]); // in the first league: BREW; later: CAST, OPPONENT_CAST, LEARN, BREW
+
+        var delta0 = int.Parse(inputs[2]); // tier-0 ingredient change
+        var delta1 = int.Parse(inputs[3]); // tier-1 ingredient change
+        var delta2 = int.Parse(inputs[4]); // tier-2 ingredient change
+        var delta3 = int.Parse(inputs[5]); // tier-3 ingredient change
+        var delta = new IngredientsVolume(delta0, delta1, delta2, delta3);
+
+        var price = int.Parse(inputs[6]); // the price in rupees if this is a potion
+
+        var
+            tomeIndex = int.Parse(
+                inputs[7]); // in the first two leagues: always 0; later: the index in the tome if this is a tome spell, equal to the read-ahead tax; For brews, this is the value of the current urgency bonus
+        var
+            taxCount = int.Parse(
+                inputs[8]); // in the first two leagues: always 0; later: the amount of taxed tier-0 ingredients you gain from learning this spell; For brews, this is how many times you can still gain an urgency bonus
+        var castable = inputs[9] != "0"; // in the first league: always 0; later: 1 if this is a castable player spell
+        var repeatable =
+            inputs[10] != "0"; // for the first two leagues: always 0; later: 1 if this is a repeatable player spell
+
+        return new GameAction(actionId, actionType, delta, price, tomeIndex, taxCount, castable, repeatable);
     }
 }
